3eea140b11b6769e80510d4b5b48efd19bffb38a
hadillarous <83988809+hadillarous@users.noreply.github.com>
2021-12-12 12:39:07 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -20,6 +20,22 @@ public class Server {
         PrintWriter outServer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socClient.getOutputStream())), true);
 
         // todo: implement the scenario
+        // Read message sent by the client
+        String v=inServer.readLine();
+        // Write message to the client
+        String voyl="";
+        String novol="";
+
+        for(int i=0;i<v.length();i++)
+        {
+                if (v.toLowerCase().charAt(i)=='a'|| v.toLowerCase().charAt(i)=='e'||v.toLowerCase().charAt(i)=='i'||v.toLowerCase().charAt(i)=='u'||v.toLowerCase().charAt(i)=='o'||v.toLowerCase().charAt(i)=='y')
+                { voyl += v.charAt(i);}
+                else
+                { novol += v.charAt(i);}
+
+
+        }
+        outServer.println(novol);
 
         // Close in / out
         inServer.close();
